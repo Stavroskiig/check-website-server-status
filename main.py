4bb4a1c7c8f3a9c4d93f0e7dbe08d117a4be0fe2
Stavros Kiigmas <stavroskiig@gmail.com>
2023-01-23 03:58:02 +0200

    Added response time

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import requests
 
+successful_requests = 0
+failed_requests = 0
+
 # Prompt the user for the website URL to monitor
 url = input("Enter the website URL to monitor: ")
+# Prompt the user for the number of requests to send
+num_requests = int(input("Enter the number of requests to send: "))
 
-# Send a GET request to the website
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-    print("Website is up and running.")
+while successful_requests + failed_requests < num_requests:
+    try:
+        # Send a GET request to the website
+        response = requests.get(url)
+        response.raise_for_status()
+        successful_requests += 1
+        print("Website is up and running.")
+        print("Response time: ", response.elapsed.total_seconds(), " seconds")
 
-# Raise an exception if the website is down
-except requests.exceptions.RequestException as e:
-    print("Website is down.")
+    # Raise an exception if the website is down
+    except requests.exceptions.RequestException as e:
+        failed_requests += 1
+        print("Website is down.")
+        print("Failed requests: ", failed_requests)
